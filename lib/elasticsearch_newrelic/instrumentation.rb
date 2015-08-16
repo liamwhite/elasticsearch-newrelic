faf62ace90bd72dccb7781f737e26adbec4f9ff8
Liam P. White <inkscapebrony@gmail.com>
2015-08-16 22:02:22 +0000

    I think that should work

--- lib/elasticsearch_newrelic/instrumentation.rb
+++ lib/elasticsearch_newrelic/instrumentation.rb
@@ -15,5 +15,27 @@ DependencyDetection.defer do
 
   executes do
     require 'new_relic/agent/datastores'
+
+    ::Elasticsearch::Model::Searching::SearchRequest.class_eval do
+      # Method to hijack
+      execute_method = :execute!
+
+      def execute_with_newrelic_trace
+        index_name = definition[:index] || 'nil'
+        result = nil
+        callback = proc do |res, metric, elapsed|
+          NewRelic::Agent::Datastores.notice_statement(index_name, elapsed)
+          result = res
+        end
+
+        NewRelic::Agent::Datastores.wrap("ElasticSearch", "search", index_name, callback) do
+          execute_without_newrelic_trace
+        end
+        result
+      end
+
+      alias_method :execute_without_newrelic_trace, execute_method
+      alias_method execute_method, :execute_with_newrelic_trace
+    end
   end
 end
